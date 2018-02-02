e5107ee931b79299d5c1cfbfe45276f85283dd4d
mikelee1 <liyuanye123456@gmail.com>
2018-02-02 20:35:21 +0800

    depart into env model run files

--- main.py
+++ main.py
@@ -1,131 +1,58 @@
-import pandas as pd
-import numpy as np
-import random
-import time,os
 from nnmodel import *
-
-
+from environment import *
 statenum = 20
-actions = ['left','right']
-episode = 10
-epsilon = 0.01#0.99
+actions = ['left', 'right']
+episode = 2000
+epsilon =1
 delt = 0.9
-treanum = 9
-def build_qtabel(actions,statenum):
-    data = np.zeros([statenum*statenum, 2])
-
-    if os.path.exists('qtable.csv'):
-        data = np.loadtxt('qtable.csv',dtype=np.float32, delimiter=",")
-
-    d = sum([[i]*statenum for i in range(statenum)],[])
-    f = sum([[i] for i in range(statenum)] * statenum,[])
-    arrays = [np.array(d), np.array(f)]
-    df = pd.DataFrame(data, index=arrays,columns=['left','right'])
-
-    return df
-
-def gene_action(s,qtable):
-
-    k = random.random()
-    if k<epsilon and qtable.iloc[s,:].all()!=0:
-
-        action = qtable.iloc[s,:].argmax()
-
-    else:
-
-        action = random.choice(actions)
-
-    return action
+robot = ['_'] * statenum
+dic = {1: 'left', 0: 'right'}
 
 
-def step(s,action,treanum,statenum):
-    r = 0
-    terminal = False
-
-    if action == 'left':
-
-        s_n = s-1
-        if s_n == treanum:
-            r = 5
-            terminal = True
-        elif s_n == -1:
-            r = -1
-            terminal = True
-
-    if action == 'right':
-        s_n = s + 1
-        if s_n == treanum:
-            r = 5
-            terminal = True
-        elif s_n >= statenum:
-            terminal = True
-            r = -1
-
-    #s_ = min(max(0,s_),5)
-    return r,s_n,terminal
-
-
-
-def update_qtable(qtable,s,s_,a,r,treanum):
-    if s_ !=treanum:
-        if s_ == statenum or s_ == -1:
-            qtable.iloc[s,:][a] = -1
-        else:
-            qtable.iloc[s,:][a] += 0.1*(r+ delt*qtable.iloc[s_,:].max() - qtable.iloc[s,:][a])
-    else:
-        qtable.iloc[s,:][a] = r
-
-
-robot = ['_']*statenum
-
-
-dic = {1:'left',0:'right'}
 def train(qtable):
     currentstate = 0
     trainlist = []
-    for i in range(episode):
-        treanum = random.randint(0, statenum - 1)
+    env = envstate(statenum,currentstate)
+    for j in range(episode):
+        treanum = env.reset()   # initiate
         print(treanum)
         while True:
+            env.refresh()
             if random.random() < epsilon:
                 action = random.choice(actions)
             else:
-                state =[[currentstate,treanum]]
-                action = qtable.evaluate(state)
+                action = qtable.evaluate([[currentstate, treanum]])
                 action = dic[action[0]]
-                #print(state,action)
 
-
-            robot = ['_'] * statenum
-
-            robot[treanum]='O'
-            robot[currentstate]='+'
-            print(''.join(robot))
-
-            r, s_n, terminal = step(currentstate, action, treanum, statenum)
+            r, s_n, terminal = env.step(currentstate, action, treanum, statenum)
+            if r > 0:
+                trainlist.append([currentstate, treanum, action])
             if terminal:
-                if r>0:
-                    trainlist.append([currentstate,treanum,action])
+
                 break
             currentstate = s_n
-
-
-        #qtable.train(trainlist)
-        if i %100 ==0:
-            pass#qtable.save()
-    #qtable.evaluate()
-    #print(trainlist)
+        qtable.train(trainlist)
 
 
 
 def evaluate(qtable):
-    qtable.evaluate()
-
+    currentstate = 0
+    env = envstate(statenum,currentstate)
+    treanum = env.reset()
+    while True:
+        env.refresh()
+        action = qtable.evaluate([[currentstate, treanum]])
+        action = dic[action[0]]
+        r, s_n, terminal = env.step(currentstate, action, treanum, statenum)
+        if terminal:
+            print('\n')
+            break
+        currentstate = s_n
 
 
 def main():
-    qtable = model(actions,statenum)
-    train1=True
+    qtable = model(actions, statenum)
+    train1 = False
     if train1:
         train(qtable)
     else:
--- nnmodel.py
+++ nnmodel.py
@@ -2,62 +2,50 @@ import tensorflow as tf
 import pandas as pd
 import numpy as np
 
-def generate_data(x):
-    x1 = [x,x+1,1,0]
-    x2 = [x+1,x,0,1]
-    return [x1,x2]
-
-
 
 class model:
-    def __init__(self,actions,statenum):
-        self.dict = {'left':-1,'right':1}
+    def __init__(self, actions, statenum):
         self.actions = actions
         self.statenum = statenum
         self.sess = tf.Session()
-        self.x = tf.placeholder(shape=[None,2],dtype=np.float32)
-        self.y = tf.placeholder(shape=[None,2],dtype=np.float32)
+        self.x = tf.placeholder(shape=[None, 2], dtype=np.float32)
+        self.y = tf.placeholder(shape=[None, 2], dtype=np.float32)
 
-        self.bias1 = tf.Variable(np.random.random(),dtype=np.float32)
-        self.layer1 = tf.Variable(tf.ones(shape=[2,2]),dtype=np.float32)
-        self.logits = tf.nn.relu(tf.matmul(self.x, self.layer1))
-        self.loss = tf.reduce_mean(-tf.reduce_sum(self.y * tf.log(self.logits),reduction_indices=[1]))
+        self.w1 = tf.Variable(tf.ones(shape=[2, 2]), dtype=np.float32)
+        self.logits = tf.nn.relu(tf.matmul(self.x, self.w1))
+        self.loss = tf.reduce_mean(-tf.reduce_sum(self.y * tf.log(self.logits), reduction_indices=[1]))
         self.optimizer = tf.train.GradientDescentOptimizer(1).minimize(self.loss)
         self.initiation = tf.global_variables_initializer()
         self.saver = tf.train.Saver()
         self.timer = 0
+        self.ckptdir = '/home/mike/Downloads/reinforelearn/qlearning_nn/ckpt/'
+        if tf.train.get_checkpoint_state(self.ckptdir):
+            self.saver.restore(self.sess, save_path=self.ckptdir)
 
-
-    def train(self,trainlist):
-        a  = pd.DataFrame(trainlist)
-        a['d']=np.nan
+    def train(self, trainlist):
+        a = pd.DataFrame(trainlist)
+        a['d'] = np.nan
         trainlist = a.values.tolist()
-        self.timer +=1
-        data =pd.DataFrame(trainlist,columns=['a','b','c','d'])
+        self.timer += 1
+        data = pd.DataFrame(trainlist, columns=['a', 'b', 'c', 'd'])
         data = data.drop_duplicates()
-        data.loc[data.c == 'left',['c','d']]=[0,1]
-        data.loc[data.c == 'right',['c','d']] =[1, 0]
+        data.loc[data.c == 'left', ['c', 'd']] = [0, 1]
+        data.loc[data.c == 'right', ['c', 'd']] = [1, 0]
 
-        x_data = np.array(data.iloc[:,:2],dtype=np.float32)
-        y_data = np.array(data.iloc[:,[2,3]],dtype=np.float32)
+        x_data = np.array(data.iloc[:, :2], dtype=np.float32)
+        y_data = np.array(data.iloc[:, [2, 3]], dtype=np.float32)
         self.trainlist = x_data
         self.y_data = y_data
 
         self.sess.run(self.initiation)
-        #print(self.sess.run(self.layer2))
-        self.sess.run(self.optimizer,feed_dict={self.x:x_data,self.y:y_data})
-        if self.timer %500 ==0:
-            print('epoch: ',self.timer)
-            print(self.sess.run(self.layer1))
+        self.sess.run(self.optimizer, feed_dict={self.x: x_data, self.y: y_data})
+        if self.timer % 500 == 0:
+            print('epoch: ', self.timer)
+            self.saver.save(self.sess, save_path=self.ckptdir)
+            print(self.sess.run(tf.argmax(self.sess.run(self.logits, feed_dict={self.x: [[2, 3]]}), 1)))
+            print(self.sess.run(tf.argmax(self.sess.run(self.logits, feed_dict={self.x: [[4, 3]]}), 1)))
 
-            self.saver.save(self.sess, save_path='/home/mike/Downloads/reinforelearn/qlearning_nn/')
-            print(self.sess.run(self.logits, feed_dict={self.x: [[2, 3]]}))
-            print(self.sess.run(self.logits, feed_dict={self.x: [[4, 3]]}))
-
-
-    def evaluate(self,state):
-        self.saver.restore(self.sess,save_path='/home/mike/Downloads/reinforelearn/qlearning_nn/')
+    def evaluate(self, state):
         a = self.sess.run(self.logits, feed_dict={self.x: state})
-        return self.sess.run(tf.argmax(a,1))
-
-
+        #print(self.sess.run(self.w1))
+        return self.sess.run(tf.argmax(a, 1))
